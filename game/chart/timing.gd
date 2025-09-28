dd8c39cbd4883331f5451ec8fec84a3310f81989
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-09-28 10:49:08 -0400

    Change judjement to use physics frame

--- game/chart/timing.gd
+++ game/chart/timing.gd
@@ -7,9 +7,12 @@ var frame_count: int = 0
 signal key_status_updated(frame: int, track_status: Array[bool])
 
 func _ready() -> void:
+	reset()
+
+func reset() -> void:
 	frame_count = 0
 
-func frame() -> void:
+func _physics_process(delta):
 	var key_status = [
 		Input.is_action_pressed('gameplay.left_top'),
 		Input.is_action_pressed('gameplay.left_middle'),
@@ -22,8 +25,9 @@ func frame() -> void:
 	for key_index in range(key_status.size()):
 		track_status[key_track_map[key_index] - 1] = key_status[key_index]
 	
-	frame_count += 1
-	key_status_updated.emit(frame_count - 1, track_status)
+	var this_frame = frame_count
+	frame_count += 1000 * delta
+	key_status_updated.emit(this_frame, track_status)
 
 func swap(track1: int, track2: int) -> void:
 	var track1_prev = key_track_map[track1 - 1]
--- menu/key_press_test.gd
+++ menu/key_press_test.gd
@@ -6,15 +6,15 @@ const parser = preload("res://game/chart/chart_parser.gd")
 
 func _ready():
 	judgement.swap(4, 6)
-	judgement.start()
+	judgement.reset()
 	
 	var chart = parser.parse_chart("res://data/charts/tutorial/easy.chart")
 	var tracks = chart.to_tracklist()
 	print(tracks)
 
-func _on_judgement_timer_key_status_updated():
-	var key_text = ""
-	for value in judgement.track_status:
+func _on_judgement_timer_key_status_updated(frame, track_status):
+	var key_text = str(frame) + ", "
+	for value in track_status:
 		key_text += "1" if value == true else "0"
 		key_text += ", "
 	self.text = key_text
